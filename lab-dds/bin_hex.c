93e3ae1cb6e3fd5ee98087e4836f58d192f21b16
rustammahilane <rustammahilane@gmail.com>
2022-10-19 08:00:43 +0530

    added bin_hex

---
+++ lab-dds/bin_hex.c
@@ -0,0 +1,41 @@
+// program to convert binary to hexadecimal number system
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int bin, dec =0;
+	printf("Enter binary: ");
+	scanf("%d", &bin);
+	
+	int temp = bin;
+	int weight = 0;
+	
+	while (temp){
+		int last_dig = temp % 10;
+		dec = dec + last_dig*pow(2, weight++);
+		temp = temp / 10;
+	}
+
+    char hex[100];
+    int i = 0;
+    while(dec){
+        temp = 0;
+        temp = dec%16;
+        
+        if(temp < 10){
+            hex[i] = temp + 48;
+            i++;
+        }
+        else
+        {
+            hex[i] = temp + 55;
+            i++;
+        }
+        dec = dec/16;
+    }
+
+    for(int j = i-1; j>= 0; j--)
+    printf("%c", hex[j]);
+	return 0;
+}
